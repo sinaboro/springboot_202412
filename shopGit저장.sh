0653d286240afd482183ea632fe5940b842d549a
sinaboro <rlaeocjf@gmail.com>
2024-12-18 12:14:51 +0900

    git 저장

---
+++ shopGit저장.sh
@@ -0,0 +1,3 @@
+git add .
+git commit -m "git 저장"
+git push origin main
--- src/main/java/com/shop/servicve/ItemImgService.java
+++ src/main/java/com/shop/servicve/ItemImgService.java
@@ -38,5 +38,5 @@ public class ItemImgService {
 
         itemImgRepository.save(itemImg);
     }
-
+    // git tset........................
 }
