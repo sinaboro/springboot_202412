f46d17e2ed2908d85d1942a8e228ac97d9b537fb
sinaboro <rlaeocjf@gmail.com>
2024-12-18 13:07:35 +0900

    git 저장

---
+++ src/main/java/com/shop/servicve/ItemService.java
@@ -0,0 +1,49 @@
+package com.shop.servicve;
+
+import com.shop.dto.ItemFormDto;
+import com.shop.entity.Item;
+import com.shop.entity.ItemImg;
+import com.shop.repository.ItemImgRepository;
+import com.shop.repository.ItemRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.List;
+
+@Service
+@Transactional
+@RequiredArgsConstructor
+@Log4j2
+public class ItemService {
+
+    private final ItemRepository itemRepository;
+    private final ItemImgService itemImgService;
+    private final ItemImgRepository itemImgRepository;
+
+    public Long saveItem(ItemFormDto itemFormDto, List<MultipartFile> itemImgFileList) throws Exception {
+
+
+        //상품등록
+        Item item = itemFormDto.createItem();
+        itemRepository.save(item);
+
+        //이미지등록
+        for(int i=0; i<itemImgFileList.size(); i++){
+            ItemImg itemImg = new ItemImg();
+
+            itemImg.setItem(item);
+
+            if(i==0){
+                itemImg.setRepimgYn("Y"); //대표이미지
+            }else{
+                itemImg.setRepimgYn("N");
+            }
+            itemImgService.saveItemImg(itemImg, itemImgFileList.get(i));
+        }
+
+        return item.getId();
+    }
+}
